203e82d7fb892f95a9acc81927cf6c2b02b4f245
SangYong-Jeong <mmaf@naver.com>
2021-12-10 10:23:18 +0900

    1교시 HighOrderComponent 공부 + 적용

---
+++ src/components/main/withBannerWrapper.js
@@ -0,0 +1,20 @@
+/* High Order Component */
+import React, { useEffect, useState } from 'react';
+import { bannerApi } from '../../modules/api';
+
+const withBannerWrapper = (OriginComponent) => {
+  const Component = (props) => {
+    const [banner, setBanner] = useState(null);
+    useEffect(() => {
+      (async () => {
+        const [data] = await bannerApi(props.id);
+        setBanner(data);
+      })();
+    }, [props.id]);
+    const combineProps = { ...props, banner };
+    return <OriginComponent className={props.className} {...combineProps} />;
+  };
+  return Component;
+};
+
+export default withBannerWrapper;
---
+++ src/components/main/withSnsWrapper.js
@@ -0,0 +1,58 @@
+const sns = {
+  ride: [
+    {
+      id: 1,
+      src: '/img/201206_1222.jpg',
+      title: 'Viet Nam',
+      content: 'Next trip: 07 March 2021 / 1 - 3 Weeks',
+    },
+    {
+      id: 2,
+      src: '/img/201206_1223.jpg',
+      title: 'Thailand',
+      content: 'Next trip: 10 May 2021 / 2 - 4 Weeks',
+    },
+    {
+      id: 3,
+      src: '/img/201206_1225.jpg',
+      title: 'Indonesia',
+      content: 'Next trip: 18 July 2021 / 3 - 5 Weeks',
+    },
+  ],
+  insta: [
+    {
+      id: 1,
+      src: '/img/201206_2333.jpg',
+      start: 4.5,
+      content:
+        'Nam tempus turpis at metus cosmo scelerisque placerat nulla deumantos solicitud de felis. Quisquemos sodales suscipit tortor condimentum. Pellentesque diam delos...',
+      wrtier: '- Annie, @annie_le, United Kingdom',
+    },
+    {
+      id: 2,
+      src: '/img/201206_2334.jpg',
+      start: 4.8,
+      content:
+        'Nam tempus turpis at metus cosmo scelerisque placerat nulla deumantos solicitud de felis. Quisquemos sodales suscipit tortor condimentum. Pellentesque diam delos...',
+      wrtier: '- John Smith, @john_smith97, Australia',
+    },
+    {
+      id: 3,
+      src: '/img/201206_2335.jpg',
+      start: 3.7,
+      content:
+        'Nam tempus turpis at metus cosmo scelerisque placerat nulla deumantos solicitud de felis. Quisquemos sodales suscipit tortor condimentum. Pellentesque diam delos...',
+      wrtier: '- Jolie, @joliee, United States',
+    },
+  ],
+};
+
+const withSnsWrapper = (OriginComponent) => {
+  const Component = (props) => {
+    const combineProps = { ...props };
+    return <OriginComponent {...combineProps} />;
+  };
+  return Component;
+};
+
+export default withSnsWrapper;
