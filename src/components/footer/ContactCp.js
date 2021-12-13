2b8735260a74c77587f99acbedaf9d4dba191fd2
SangYong-Jeong <mmaf@naver.com>
2021-12-13 09:51:27 +0900

    Footer layout 작업중

--- src/components/footer/ContactCp.js
+++ src/components/footer/ContactCp.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import styled, { color } from '../../style';
 
+import LogoCp from '../common/LogoCp';
+
 const Wrapper = styled.div``;
 
 const ContactCp = () => {
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <LogoCp type="W" />
+    </Wrapper>
+  );
 };
 
 export default React.memo(ContactCp);
--- src/components/footer/FooterWrapperCp.js
+++ src/components/footer/FooterWrapperCp.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import styled, { color, Container } from '../../style';
 
+import ContactCp from './ContactCp';
+import MenuWrapCp from './MenuWrapCp';
+import EmailCp from './EmailCp';
+import CopyrightCp from './CopyrightCp';
+import CardCp from './CardCp';
+
 const Wrapper = styled.footer`
   background-color: #1a1a1a;
   color: #fff;
 `;
 
+const Wrap = styled(Container)`
+  display: flex;
+`;
+
 const FooterWrapperCp = () => {
   return (
     <Wrapper>
-      <Container></Container>
+      <Wrap>
+        <ContactCp />
+        <MenuWrapCp />
+        <EmailCp />
+      </Wrap>
+      <Container>
+        <CopyrightCp />
+        <CardCp />
+      </Container>
     </Wrapper>
   );
 };
