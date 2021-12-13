8ec8ef14a2229878192069d19caaf167c354d4b5
SangYong-Jeong <mmaf@naver.com>
2021-12-13 10:30:43 +0900

    ContactCp layout 구현 완료

--- src/components/footer/ContactCp.js
+++ src/components/footer/ContactCp.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import styled, { color } from '../../style';
+import styled, { color, css } from '../../style';
 
 import LogoCp from '../common/LogoCp';
+import ContentCp from '../common/ContentCp';
+import GapCp from '../common/GapCp';
 
 const Wrapper = styled.div``;
 
@@ -9,6 +11,13 @@ const ContactCp = () => {
   return (
     <Wrapper>
       <LogoCp type="W" />
+      <GapCp height="2em" />
+      <ContentCp txt="685 Market Street" />
+      <ContentCp txt="San Francisco, CA 94105," />
+      <ContentCp txt="United States" />
+      <GapCp height="2em" />
+      <ContentCp txt="Call us: 1.800.000.6690" />
+      <ContentCp txt="Email: support@halothemes.com" link="/" />
     </Wrapper>
   );
 };
